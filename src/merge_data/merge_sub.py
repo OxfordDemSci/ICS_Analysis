e403839bfdd7c5f9ad051c3f0483c39a52283863
MarkDVerhagen <markdverhagen@gmail.com>
2023-03-09 12:49:37 +0100

    added scripts to compile a final sub table

---
+++ src/merge_data/merge_sub.py
@@ -0,0 +1,31 @@
+#!interpreter [optional-arg]
+# -*- coding: utf-8 -*-
+
+"""
+{Script to combine all submission level data into single analysis file}
+
+"""
+
+import pandas as pd
+import os
+
+def main():
+
+    ## Set paths
+    edit_path = os.path.join(os.getcwd(), '..', '..', 'data', 'edit')
+    final_path = os.path.join(os.getcwd(), '..', '..', 'data', 'edit')
+
+    ## Read data
+    sub = pd.read_pickle(os.path.join(edit_path, 'sub_table.pkl'))
+    ics_to_sub = pd.read_pickle(os.path.join(edit_path, 'ics_to_sub_table.pkl'))
+
+    ## Merge data
+    sub_final = pd.merge(sub, ics_to_sub)
+
+    assert sub.shape[0] == sub_final.shape[0]
+
+    ## Write data
+    sub_final.to_pickle(os.path.join(final_path, 'sub_table.pkl'))
+
+if __name__ == "__main__":
+    main()
