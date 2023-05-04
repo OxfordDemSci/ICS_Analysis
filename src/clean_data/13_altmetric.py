660d87e85cba5649323528c55474ec942dc890e2
MarkDVerhagen <markdverhagen@gmail.com>
2023-05-04 15:14:46 +0200

    File structure to reflect data collection pipeline

---
+++ src/clean_data/13_altmetric.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+## Read UOA_id and INST_id to DOI crosswalk
+
+## Merge in average REF scores
+
+## Simulate altmetric based on linear trend with REF scores plus white noise
+
+## Simulate impact factor based on linear trend with REF scores plus white noise
+
+## Randomly simulate number of authors
