8af4d755d41ff4cdcd69fcf67db3c6e852a8ac63
GISRedeDev <dm@gisrede.com>
2023-07-16 15:58:31 +0100

    Add ics_table_for_download and adjusted UOA table

--- src/dashboard/api/app/models.py
+++ src/dashboard/api/app/models.py
@@ -17,6 +17,49 @@ class ICS(Base):
         Index('idx_ics_id_ics_id', 'id', 'ics_id'),
     )
 
+class ICSTableForDownload(Base):
+    __tablename__ = "ics_table_for_download"
+
+    id = Column(Integer)
+    ukprn = Column(Integer)
+    institution_name = Column(String)
+    main_panel = Column(String)
+    unit_of_assessment_number = Column(String)
+    unit_of_assessment_name = Column(String)
+    multiple_submission_letter = Column(String)
+    multiple_submission_name = Column(String)
+    joint_submission = Column(String)
+    ics_id = Column(String)
+    title = Column(String)
+    is_continued_from_2014 = Column(String)
+    summary_impact_type = Column(String)
+    countries = Column(String)
+    formal_partners = Column(String)
+    funding_programmes = Column(String)
+    global_research_identifiers = Column(String)
+    funders = Column(String)
+    researcher_orcids = Column(String)
+    grant_funding = Column(String)
+    summary_of_the_impact = Column(String)
+    underpinning_research = Column(String)
+    references_to_the_research = Column(String)
+    details_of_the_impact = Column(String)
+    sources_to_corroborate_the_impact = Column(String)
+    covid_statement = Column(String)
+    uoa = Column(String)
+    summary_of_the_impact_topic = Column(String)
+    cultural = Column(String)
+    economic = Column(String)
+    environmental = Column(String)
+    health = Column(String)
+    legal = Column(String)
+    political = Column(String)
+    societal = Column(String)
+    technological = Column(String)
+    inst_postcode = Column(String)
+    inst_postcode_district = Column(String)
+    postcode = Column(String)
+
 
 class Topics(Base):
     __tablename__ = "topics"
