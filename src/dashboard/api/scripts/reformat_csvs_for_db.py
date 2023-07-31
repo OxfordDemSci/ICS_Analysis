c6f2f1fcb6e098b9e45e42d3e634c58be9245a68
GISRedeDev <dm@gisrede.com>
2023-07-31 20:03:01 +0100

    Script to make topics and weights tables

--- src/dashboard/api/scripts/reformat_csvs_for_db.py
+++ src/dashboard/api/scripts/reformat_csvs_for_db.py
@@ -7,6 +7,13 @@ if not ENRICHED_ICS_TABLE.exists():
     raise FileNotFoundError(f'{str(ENRICHED_ICS_TABLE)} is not in place. This file is not held in github and needs to be in {str(ENRICHED_ICS_TABLE.parent)}')
 OUTPUT_ICS_TABLE = BASE.joinpath('db-data/ICS_DATABASE_TABLE.csv')
 
+
+TOPICS_DIR = BASE.parent.parent.parent.parent.parent.joinpath('data/dashboard/nn3nn7')
+TOPICS_TABLE = TOPICS_DIR.joinpath('topics.xlsx')
+WEIGHTS_TABLE = TOPICS_DIR.joinpath('candidate_nn3nn7.xlsx')
+TOPICS_OUT = BASE.joinpath('db-data/TOPICS_TABLE.csv')
+TOPICS_WEIGHTS_OUT = BASE.joinpath('db-data/TOPIC_WEIGHTS_TABLE.csv')
+
 columns_to_keep = [
     'id',
     'ukprn',
@@ -61,6 +68,23 @@ def make_ics_table():
     ics_df = ics_df[columns_to_keep]
     ics_df.to_csv(OUTPUT_ICS_TABLE, index=False)
 
+def make_topics_and_weights():
+    weights_df = pd.read_excel(WEIGHTS_TABLE, sheet_name='Sheet1')
+    topics_df = pd.read_excel(TOPICS_TABLE, sheet_name='Sheet1')
+    weights_df = weights_df.rename(columns={'REF impact case study identifier': 'ics_id'})
+    cols = [x for x in weights_df.columns if isinstance(x, int)]
+    cols.insert(0, 'ics_id')
+    weights_df = weights_df[cols]
+    weights_df = weights_df.fillna(0)
+    df_long = pd.melt(weights_df, id_vars=['ics_id'], var_name='topic_id', value_name='probability')
+    df_long['id'] = df_long.index.copy().astype('int')
+    df_long = df_long[['id', 'ics_id', 'topic_id', 'probability']]
+    df_long.to_csv(TOPICS_WEIGHTS_OUT, index=False)
+    topics_df.columns = topics_df.columns.str.lower().str.replace(' ', '_')
+    topics_df.to_csv(TOPICS_OUT, index=False)
 
 if __name__ == "__main__":
-    make_ics_table()
+    print('Making ICS table')
+    #make_ics_table()
+    print('Reformatting topics and weights')
+    make_topics_and_weights()
