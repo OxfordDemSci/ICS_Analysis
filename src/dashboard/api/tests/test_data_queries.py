8305ca941cd05b53f12e2c9ceaf81909fcaa4e24
GISRedeDev <dm@gisrede.com>
2023-08-06 14:26:39 +0100

    test get_topic_groups

--- src/dashboard/api/tests/test_data_queries.py
+++ src/dashboard/api/tests/test_data_queries.py
@@ -4,14 +4,27 @@ from pathlib import Path
 
 from app.data_queries import (
     get_topics,
+    get_topic_groups,
 )
 
 TEST_DATA = Path(__file__).resolve().parent.joinpath('test_data')
 
+# @pytest.fixture
+# def topics_df():
+#     df = pd.read_csv(TEST_DATA.joinpath('TOPICS_TABLE.csv'))
+#     yield df
+
+# @pytest.fixture
+# def topics_groups_df():
+#     df = pd.read_csv(TEST_DATA.joinpath("TOPIC_GROUPS_TABLE.csv"))
+#     yield df
+
 @pytest.fixture
-def topics_df():
-    df = pd.read_csv(TEST_DATA.joinpath('TOPICS_TABLE.csv'))
-    yield df
+def dataframes():
+    dfs = {}
+    for csv in TEST_DATA.iterdir():
+        dfs[csv.stem] = pd.read_csv(csv)
+    yield dfs
 
 @pytest.mark.parametrize("topic", [
     (None),
@@ -20,7 +33,8 @@ def topics_df():
     ("Gaelic Preservation"),
     ("Cultural Engagement")
 ])
-def test_get_topics(session, topics_df, topic):
+def test_get_topics(session, dataframes, topic):
+    topics_df = dataframes["TOPICS_TABLE"]
     topics = get_topics(topic=topic)
     if topic is not None:
         topics_df = topics_df[topics_df.topic_name == topic]
@@ -34,4 +48,8 @@ def test_get_topics(session, topics_df, topic):
         assert "All Topics" in [x["topic_name"] for x in topics]
 
 
-
+def test_get_topic_groups(session, dataframes):
+    topic_groups_df = dataframes["TOPIC_GROUPS_TABLE"]
+    topic_groups = get_topic_groups()
+    topic_groups = [x["topic_group"] for x in topic_groups]
+    assert sorted(topic_groups) == sorted(topic_groups_df.topic_group.tolist())
