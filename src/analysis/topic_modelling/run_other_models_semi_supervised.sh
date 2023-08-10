9bb35fce2fd31f86de22ea23ef523962b61c4adf
Linda Li <linda_l97@hotmail.com>
2023-08-10 15:57:24 +0100

    Update topic modelling pipeline into .py scripts

--- src/analysis/topic_modelling/run_other_models_semi_supervised.sh
+++
@@ -1,5 +0,0 @@
-python3 BERT_other_model_variants_semi_supervised.py "./data/subset/non_shape.csv" "BERT_non_SHAPE_semi_supervised" "Summary impact type";
-python3 BERT_other_model_variants_semi_supervised.py "./data/subset/panel_d.csv" "BERT_panel_D_semi_supervised" "Summary impact type";
-python3 BERT_other_model_variants_semi_supervised.py "./data/subset/panel_c.csv" "BERT_panel_C_semi_supervised" "Summary impact type";
-python3 BERT_other_model_variants_semi_supervised.py "./data/clean_ref_ics_data_cleaned_lemmatized_sample.csv" "BERT_SHAPE_semi_supervised" "Summary impact type";
-python3 BERT_other_model_variants_semi_supervised.py "./data/clean_ref_ics_data_lemmatized_full.csv" "BERT_full_semi_supervised" "Summary impact type";
