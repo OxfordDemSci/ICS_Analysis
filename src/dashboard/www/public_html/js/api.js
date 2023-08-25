2d21fbf4229e41f914ac039737512cf490ae79ea
Doug Leasure <doug.leasure@protonmail.com>
2023-08-25 13:21:26 +0100

    update postcode geometries

--- src/dashboard/www/public_html/js/api.js
+++ src/dashboard/www/public_html/js/api.js
@@ -118,7 +118,7 @@ export function getUKPostCodeAreasBoundary() {
 
     var result = "";
     $.ajax({
-        url: "./data/UK_postcode_area.geojson",
+        url: "./data/UK_postcode_area_v7.geojson",
         async: false,
         type: 'get',
         dataType: 'json',
