ebaf8450bff9c07d7ce476f5d6d11191214ea922
crahal <charlierahal@gmail.com>
2024-01-15 21:46:11 +0000

    Create tables 1-6 via some refactoring. Closes #63.

--- src/analysis/make_all_analysis.py
+++ src/analysis/make_all_analysis.py
@@ -11,6 +11,11 @@ from figure_plotters.make_figure_eighteen import make_figure_eighteen
 from figure_plotters.make_figure_nineteen import make_figure_nineteen
 from helpers.cluster_by_cluster import (make_cluster_figure,
                                         make_descriptives)
+from table_makers.table_makers import (make_table_one,
+                                       make_table_two,
+                                       make_table_four,
+                                       make_table_five,
+                                       make_table_six)
 
 def prep_data():
     df_ref = pd.read_csv(os.path.join(os.getcwd(),
@@ -19,7 +24,10 @@ def prep_data():
                                       'data',
                                       'final',
                                       'enhanced_ref_data.csv'),
-                         usecols=['Unit of assessment number',
+                         usecols=['Unit of assessment name',
+                                  'fte',
+                                  'num_doc_degrees_total',
+                                  'tot_income',
                                   'REF impact case study identifier',
                                   'funders_extracted',
                                   'countries_extracted',
@@ -49,16 +57,22 @@ def prep_data():
 
 if __name__ == "__main__":
     mpl.use('Agg')
-    print('Making all Figures: Beginning!')
+#    print('Making all Figures: Beginning!')
 #    make_figure_two()
 #    make_figure_three()
 #    df, paper_level = prep_data()
+    make_table_one(os.path.join('..', '..', 'tables'))
+    make_table_two(os.path.join('..', '..', 'tables'))
+
 #    for cluster in range(1, 11):
 #        make_cluster_figure(df, paper_level, int(cluster))
 #        make_descriptives(df, paper_level, int(cluster))
-    make_figure_fourteen()
-    make_figure_fifteen()
-    make_figure_sixteen()
-    make_figure_seventeen()
-    make_figure_eighteen()
-    make_figure_nineteen()
+    make_table_four(os.path.join('..', '..', 'tables'))
+#    make_figure_fourteen()
+    make_table_five(os.path.join('..', '..', 'tables'))
+#    make_figure_fifteen()
+    make_table_six(os.path.join('..', '..', 'tables'))
+#    make_figure_sixteen()
+#    make_figure_seventeen()
+#    make_figure_eighteen()
+#    make_figure_nineteen()
